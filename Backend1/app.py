55dc27225de9b7d27374f2eb8b34677358646aad
AtharvaP09 <atharvapatil909@gmail.com>
2025-08-27 14:28:09 +0530

    Created a demo login user registrtation form and database to input data.

--- Backend1/app.py
+++ Backend1/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+from models import db
 
 app = Flask(__name__)
+CORS(app)
 
+#Database 
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///project.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 #routes
 from routes import *
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import UserRegistration from "./Pages/UserRegistration";
 
 function App() {
 
   return (
     <>
       <h3>Secure Token Based Malpractice Prevention , Hello World!!</h3>
+      <UserRegistration />
     </>
   )
 }
